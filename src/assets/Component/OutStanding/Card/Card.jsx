36d4785b7016a1ede78252e6346e61ee10c338c1
Tuna65 <patuan657@gmail.com>
2022-06-16 21:46:39 +0700

    fix

--- src/assets/Component/OutStanding/Card/Card.jsx
+++ src/assets/Component/OutStanding/Card/Card.jsx
@@ -3,16 +3,19 @@ import Layout from '../../Layout/Layout';
 
 import './Card.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 function Card({ img, icon, urlImg, urlVideo }) {
+    const a = useRef();
+
     return (
-        <div className="o__card">
+        <div className="o__card" ref={a}>
             <div className="o__img">
                 <img src={img} alt="" />
                 <span className="o__icon">
                     <FontAwesomeIcon
                         icon={icon}
                         onClick={() => {
-                            render(<Layout urlImg={urlImg} urlVideo={urlVideo} />);
+                            render(<Layout urlImg={urlImg} urlVideo={urlVideo} />, a.current);
                         }}
                     />
                 </span>
